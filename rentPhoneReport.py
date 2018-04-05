feba3f14e060429f575548eabed0dd2b5fafc376
kylin <975168367@qq.com>
2018-04-05 16:54:57 +0800

    V1.0版本完成，当前服务器能够提供接口，使用sqllite数据库存储客户端提交的数据，当前还不具备后台管理功能，预计V2.0版本添加管理台功能

--- rentPhoneReport.py
+++ rentPhoneReport.py
@@ -26,26 +26,42 @@ def teardown_request(exception):
 @app.route('/rentPhoneReport',methods=['POST','GET'])
 def hello_world():
     if request.form.get("method",'getNoMethod') == "rent":
-        phone,name,deviceId,rentPhoneType = request.form.get("phone",'getNoPhone'),request.form.get("name",'getNoName'),request.form.get("deviceId",'getNodeviceId'),request.form.get("phoneType",'getNophoneType');
-
+        phone,name,deviceId,rentPhoneType,battaryLevel,gameboxVersion,hiappVersion,hmsVersion = request.form.get("phone",'getNoPhone'),                                                                                            request.form.get("name",'getNoName'),\
+                                                                                                request.form.get("deviceId",'getNodeviceId'),\
+                                                                                                request.form.get("phoneType",'getNophoneType'),\
+                                                                                                request.form.get("battaryLevel",'getNobattaryLevel'),\
+                                                                                                request.form.get("gameboxVersion",'getNogameboxVersion'),\
+                                                                                                request.form.get("hiappVersion",'getNohiappVersion'),\
+                                                                                                request.form.get("hmsVersion",'getNohmsVersion');
         print(request.method)
         print(request.form.get("phone",'getNoPhone'),
               request.form.get("name",'getNoName'),
               request.form.get("deviceId",'getNodeviceId'),
               request.form.get("phoneType",'getNophoneType'),)
-        rentSql = "INSERT INTO rentLog (name, phone,deviceId,rentPhoneType,status) VALUES (?,?,?,?,?)"
-        g.db.execute(rentSql,(name, phone,deviceId,rentPhoneType,1))
+        rentSql = u"INSERT INTO rentLog (name, phone,deviceId,rentPhoneType,status,battaryLevel,gameboxVersion,hiappVersion,hmsVersion) " \
+                  u"VALUES ('{name}','{phone}','{deviceId}','{rentPhoneType}','{status}','{battaryLevel}','{gameboxVersion}','{hiappVersion}','{hmsVersion}')".format(
+            name=name,phone=phone,deviceId=deviceId,rentPhoneType=rentPhoneType,status=1,battaryLevel=battaryLevel,gameboxVersion=gameboxVersion,hiappVersion=hiappVersion,hmsVersion=hmsVersion
+        )
+        g.db.execute(rentSql)
         g.db.commit()
     if request.form.get("method",'getNoMethod') == "return":
-        phone,name,deviceId,rentPhoneType = request.form.get("phone",'getNoPhone'),request.form.get("name",'getNoName'),request.form.get("deviceId",'getNodeviceId'),request.form.get("phoneType",'getNophoneType');
+        phone,name,deviceId,rentPhoneType,battaryLevel,gameboxVersion,hiappVersion,hmsVersion = request.form.get("phone",'getNoPhone'),                                                                                            request.form.get("name",'getNoName'),\
+                                                                                                request.form.get("deviceId",'getNodeviceId'),\
+                                                                                                request.form.get("phoneType",'getNophoneType'),\
+                                                                                                request.form.get("battaryLevel",'getNobattaryLevel'),\
+                                                                                                request.form.get("gameboxVersion",'getNogameboxVersion'),\
+                                                                                                request.form.get("hiappVersion",'getNohiappVersion'),\
+                                                                                                request.form.get("hmsVersion",'getNohmsVersion');
 
         print(request.method)
         print(request.form.get("phone",'getNoPhone'),
               request.form.get("name",'getNoName'),
               request.form.get("deviceId",'getNodeviceId'),
               request.form.get("phoneType",'getNophoneType'),)
-        rentSql = "INSERT INTO rentLog (name, phone,deviceId,rentPhoneType,status) VALUES (?,?,?,?,?)"
-        g.db.execute(rentSql,(name, phone,deviceId,rentPhoneType,1))
+        returnSql = u"UPDATE rentLog set returnName='{returnName}',returnPhone='{returnPhone}',status={status},returnTime=(datetime(CURRENT_TIMESTAMP,'localtime')),battaryLevel='{battaryLevel}',gameboxVersion='{gameboxVersion}',hiappVersion='{hiappVersion}',hmsVersion='{hmsVersion}' " \
+                    u"where status=1 and deviceId = '{deviceId}'".format(returnName=name,status=0,returnPhone=phone,deviceId=deviceId,battaryLevel=battaryLevel,gameboxVersion=gameboxVersion,hiappVersion=hiappVersion,hmsVersion=hmsVersion);
+        print returnSql
+        g.db.execute(returnSql)
         g.db.commit()
 
 
@@ -55,4 +71,4 @@ def hello_world():
 
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0",port=8888,debug=True)
+    app.run(host="0.0.0.0",port=9999,debug=True)
--- v1.0.sql
+++ v1.0.sql
@@ -3,15 +3,25 @@ CREATE TABLE rentLog(
 id INTEGER PRIMARY KEY AUTOINCREMENT,
 name TEXT,-- 使用者姓名
 phone TEXT,--   使用者的电话号码，方便管理者联系到使用者
+
+
+
+
 rentPhoneType TEXT,----当前手机的型号，如荣耀V8等
 deviceId TEXT,----当前手机的imei号
 status int,--当前手机状态，0表示已结束流程，1表示借出状态
+  returnName TEXT,-- 使用者姓名
+returnPhone TEXT,--   使用者的电话号码，方便管理者联系到使用者
 
 renttime DATA DEFAULT (datetime(CURRENT_TIMESTAMP,'localtime')),
-returntime DAT,
+returntime DATA,
 battaryLevel int,---当前手机的电量信息
 gameboxVersion TEXT, ---当前手机的游戏中心版本号信息
 hiappVersion  TEXT, --当前手机应用市场的版本号信息
 hmsVersion   TEXT  ---当前手机华为移动服务的版本号信息
 );
 
+
+
+UPDATE rentLog set returnName='饶麒麟',returnPhone='18551789470',status=0,returntime=(datetime(CURRENT_TIMESTAMP,'localtime'),battaryLevel='getNobattaryLevel',gameboxVersion='getNogameboxVersion',hiappVersion='getNohiappVersion',hmsVersion='getNohmsVersion')
+;
